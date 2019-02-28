6a16a13bab9c154da6fd48b69c487d0b50c9338f
GugaGongadze <ggong13@freeuni.edu.ge>
2019-02-28 11:32:55 +0400

    Initial Commit

---
+++ Project2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Guga Gongadze on 2/28/19.
+//  Copyright © 2019 Guga Gongadze. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
